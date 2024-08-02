0e81614259bf632a4ef55a48a550f8b6fc2f933a
enockDev <enockdev01@gmail.com>
2024-08-02 14:58:28 +0200

    deployment config

--- src/message.tsx
+++
@@ -1,9 +0,0 @@
-function Message() {
-    const name = 'Enock';
-    let mess = <h1>Hello {name}</h1>
-    if (name)
-        return mess;
-    return 'Hello World!'
-};
-
-export default Message;
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/Community_Management/",
   plugins: [react()],
   server: {
-    port: 3001, // Change the port to 3001 or any other available port
+    port: 3001, 
   },
 });
